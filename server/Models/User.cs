9a2beba146e9bc9aa01ad820febeccb1c32872ce
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2020-03-20 17:22:29 +0300

    feat(server): Added IsAdmin getter for User

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -11,5 +11,7 @@ namespace server.Models
 
         public int? DepartmentId { get; set; }
         public virtual Department Department { get; set; }
+
+        public bool IsAdmin => this.DepartmentId == null;
     }
 }
