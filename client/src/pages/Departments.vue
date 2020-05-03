8d2768c82b92ad23b3bc5fe013fc3318b4082724
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2020-05-03 23:13:28 +0300

    refactor: Extracted REST API consuming logic to a service

--- client/src/pages/Departments.vue
+++ client/src/pages/Departments.vue
@@ -84,39 +84,7 @@ import { ref, reactive, computed, onMounted, toRefs } from '@vue/composition-api
 import { useForm } from '../composition/useForm'
 import { useServerSideProcessedTable } from '../composition/useServerSideProcessedTable'
 
-async function fetchDataFromServer ({ startRow, count, search, sortBy, descending }) {
-  const params = new URLSearchParams({
-    $skip: startRow,
-    $top: count,
-    $count: true
-  })
-
-  if (sortBy) {
-    params.append('$orderBy', `${sortBy} ${descending ? 'desc' : 'asc'}`)
-  }
-
-  if (search?.fields?.length > 0 && search?.term) {
-    const filterQuery = search.fields
-      .map(field => `contains(${field}, '${search.term}')`)
-      .join(' or ')
-
-    params.append('$filter', filterQuery)
-  }
-
-  const url = `/api/Department?${params.toString()}`
-
-  const response = await axios(url)
-
-  return response.data
-}
-
-async function pushDataToServer (data, isUpdating) {
-  return axios({
-    url: `/api/Department/${isUpdating ? data.Id : ''}`,
-    method: isUpdating ? 'PUT' : 'POST',
-    data
-  })
-}
+import { fetchDataFromServer, pushDataToServer } from '../services/ApiService'
 
 export default {
   name: 'DepartmentsPage',
