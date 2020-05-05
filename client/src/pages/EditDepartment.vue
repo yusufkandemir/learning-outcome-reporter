c1f2afcf89a337a1a093ae944716c42ee5ffddc9
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2020-05-05 04:39:50 +0300

    feat: Added edit department form

--- client/src/pages/EditDepartment.vue
+++ client/src/pages/EditDepartment.vue
@@ -1,5 +1,22 @@
 <template>
-  <q-page class="flex flex-center" padding>
+  <q-page class="flex row justify-center content-start items-center q-col-gutter-lg" padding>
+    <div class="row justify-center col-md-12 col-lg-4">
+      <div class="col-12">
+        <q-card class="q-pa-sm">
+          <q-card-section>
+            <span class="text-h5">Edit Department</span>
+          </q-card-section>
+
+          <q-card-section>
+            <q-input v-model="department.Name" label="Name" :loading="loading"></q-input>
+          </q-card-section>
+
+          <q-card-actions class="justify-end">
+            <q-btn color="primary" @click="onUpdate" :loading="loading">Update</q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
     <o-crud-table
       class="q-my-lg col-md-12 col-lg-8"
       :entity="entity"
@@ -17,9 +34,12 @@
 </template>
 
 <script>
-import { ref, reactive } from '@vue/composition-api'
+import { ref, reactive, onMounted } from '@vue/composition-api'
+import axios from 'axios'
+import { Notify } from 'quasar'
 
 import OCrudTable from '../components/OCrudTable'
+import { pushDataToServer } from '../services/ApiService'
 
 export default {
   name: 'EditDepartmentPage',
@@ -79,13 +99,81 @@ export default {
       }
     }
 
+    const { loading, onUpdate, department } = useUpdateForm(departmentId)
+
     return {
       items,
       columns,
       pagination,
       entity,
-      actionConfig
+      actionConfig,
+
+      loading,
+      onUpdate,
+      department
     }
+  }
+}
+
+function useUpdateForm (departmentId) {
+  const loading = ref(false)
+
+  const department = reactive({
+    Id: 0,
+    Name: ''
+  })
+
+  const path = `Department/${departmentId}`
+
+  onMounted(async () => {
+    loading.value = true
+
+    try {
+      const { data } = await axios(`/api/${path}`)
+      Object.assign(department, data)
+    } catch (error) {
+      Notify.create({
+        type: 'negative',
+        position: 'top',
+        message: 'An error occured while fetching the data',
+        caption: error.message
+      })
+
+      return
+    } finally {
+      loading.value = false
+    }
+  })
+
+  const onUpdate = async () => {
+    loading.value = true
+
+    try {
+      await pushDataToServer(path, department, true)
+    } catch (error) {
+      Notify.create({
+        type: 'negative',
+        position: 'top',
+        message: 'An error occured while updating',
+        caption: error.message
+      })
+
+      return
+    } finally {
+      loading.value = false
+    }
+
+    Notify.create({
+      type: 'positive',
+      position: 'top',
+      message: 'Update successful'
+    })
+  }
+
+  return {
+    loading,
+    onUpdate,
+    department
   }
 }
 </script>
