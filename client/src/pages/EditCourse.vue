b0ac511d148b25bb5c0167a9bbae029ffd28c659
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2020-05-07 04:23:23 +0300

    refactor(client/EditCoursePage): Extracted table logic to a function

--- client/src/pages/EditCourse.vue
+++ client/src/pages/EditCourse.vue
@@ -2,14 +2,14 @@
   <q-page class="flex flex-center" padding>
     <div class="q-my-lg">
       <o-crud-table
-        :entity="entity"
-        :data="items"
-        :columns="columns"
-        :pagination="pagination"
-        :actions="actionConfig"
+        :entity="assignmentTable.entity"
+        :data="assignmentTable.items"
+        :columns="assignmentTable.columns"
+        :pagination="assignmentTable.pagination"
+        :actions="assignmentTable.actions"
       >
         <template v-slot:form="{ item }">
-          <q-select v-model="item.Type" :options="assignmentTypes" label="Type"></q-select>
+          <q-select v-model="item.Type" :options="assignmentTable.assignmentTypes" label="Type"></q-select>
           <q-input v-model.number="item.Weight" type="number" min="0" max="1" label="Weight"></q-input>
         </template>
       </o-crud-table>
@@ -29,69 +29,78 @@ export default defineComponent({
     OCrudTable
   },
   setup (props, context) {
-    const items = ref([])
-    const columns = ref([
-      {
-        name: 'type',
-        label: 'Type',
-        field: 'Type',
-        sortable: false,
-        searchable: false
-      },
-      {
-        name: 'weight',
-        label: 'Weight',
-        field: 'Weight',
-        sortable: false,
-        searchable: false
-      },
-      { name: 'actions', label: 'Actions', align: 'right' }
-    ])
-    const pagination = reactive({
-      page: 1,
-      sortBy: 'type',
-      descending: true,
-      rowsPerPage: context.root.$q.screen.xs ? 12 : 24,
-      rowsNumber: 0
-    })
-
-    const assignmentTypes = ['Midterm', 'Final', 'Project', 'LabExam', 'Assignment', 'Other']
-
-    const { courseId, courseInfoId } = context.root.$route.params
-
-    const assignmentsService = new ODataApiService(`/api/Course/${courseId}/Assignments`)
-    const entity = {
-      key: 'Id',
-      name: 'Assignment',
-      displayName: (plural = false) => `Assignment${plural ? 's' : ''}`,
-      route: (key = '') => `/course_info/${courseInfoId}/courses/${courseId}/assignments/${key}`,
-      service: assignmentsService,
-      defaultValue () {
-        return {
-          Id: 0,
-          Type: '',
-          Weight: '1'
-        }
-      }
-    }
-
-    const actionConfig = {
-      create: {
-        icon: 'mdi-clipboard-check'
-      },
-      search: {
-        enabled: false
-      }
-    }
+    const assignmentTable = useAssignmentTable(context)
 
     return {
-      items,
-      columns,
-      pagination,
-      entity,
-      actionConfig,
-      assignmentTypes
+      assignmentTable: ref(assignmentTable)
     }
   }
 })
+
+function useAssignmentTable (context) {
+  const items = ref([])
+  const columns = ref([
+    {
+      name: 'type',
+      label: 'Type',
+      field: 'Type',
+      sortable: false,
+      searchable: false
+    },
+    {
+      name: 'weight',
+      label: 'Weight',
+      field: 'Weight',
+      sortable: false,
+      searchable: false
+    },
+    { name: 'actions', label: 'Actions', align: 'right' }
+  ])
+  const pagination = reactive({
+    page: 1,
+    sortBy: 'type',
+    descending: true,
+    rowsPerPage: context.root.$q.screen.xs ? 12 : 24,
+    rowsNumber: 0
+  })
+
+  const { courseId, courseInfoId } = context.root.$route.params
+
+  const assignmentsService = new ODataApiService(`/api/Course/${courseId}/Assignments`)
+  const entity = {
+    key: 'Id',
+    name: 'Assignment',
+    displayName: (plural = false) => `Assignment${plural ? 's' : ''}`,
+    route: (key = '') => `/course_info/${courseInfoId}/courses/${courseId}/assignments/${key}`,
+    service: assignmentsService,
+    defaultValue () {
+      return {
+        Id: 0,
+        Type: '',
+        Weight: '1'
+      }
+    }
+  }
+
+  const actions = {
+    create: {
+      icon: 'mdi-clipboard-check'
+    },
+    search: {
+      enabled: false
+    }
+  }
+
+  const assignmentTypes = ['Midterm', 'Final', 'Project', 'LabExam', 'Assignment', 'Other']
+
+  return {
+    items,
+    columns,
+    pagination,
+    entity,
+    actions,
+
+    assignmentTypes
+  }
+}
 </script>
