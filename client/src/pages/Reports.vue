2dd3eb4e98fab70518a2be82b04aecb59677baba
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2020-05-15 07:43:21 +0300

    feat(client): Added form for generating the report with different parameters

--- client/src/pages/Reports.vue
+++ client/src/pages/Reports.vue
@@ -1,7 +1,33 @@
 <template>
   <q-page class="flex row justify-center content-start items-center q-col-gutter-lg" padding>
     <div class="row justify-center col-12">
-      <div class="col-6">
+      <div class="col-12 col-md-8 col-lg-4">
+        <q-card class="q-pa-sm">
+          <q-card-section>
+            <span class="text-h5">Department Report</span>
+          </q-card-section>
+
+          <q-card-section>
+            <q-input
+              v-model.number="form.departmentId"
+              label="Deparment Id"
+              type="number"
+              min="1"
+              :loading="loading"
+            ></q-input>
+            <q-select v-model="form.semester" :options="semesters" label="Semester" />
+            <q-input v-model.number="form.year" label="Year" type="number"></q-input>
+          </q-card-section>
+
+          <q-card-actions class="justify-end">
+            <q-btn color="primary" @click="onGenerate" :loading="loading">Generate</q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+
+    <div class="row justify-center col-12">
+      <div class="col-6" v-if="showingChart">
         <apexchart type="bar" :options="options" :series="series"></apexchart>
         <q-inner-loading :showing="loading">
           <q-spinner-gears size="xl" color="primary" />
@@ -12,7 +38,7 @@
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from '@vue/composition-api'
+import { defineComponent, ref, reactive } from '@vue/composition-api'
 import axios from 'axios'
 import { Notify } from 'quasar'
 
@@ -21,6 +47,7 @@ export default defineComponent({
   setup (props, context) {
     const loading = ref(false)
 
+    const showingChart = ref(false)
     const series = ref([{
       name: 'Avg. Performance',
       data: []
@@ -74,32 +101,55 @@ export default defineComponent({
       }
     }
 
-    onMounted(async () => {
+    const form = reactive({
+      departmentId: null,
+      semester: null,
+      year: null
+    })
+
+    const onGenerate = async () => {
       loading.value = true
 
       try {
-        const response = await axios.get('/api/Reports/Department?DepartmentId=1&Semester=Fall&Year=2020')
+        const params = new URLSearchParams({
+          DepartmentId: form.departmentId,
+          Semester: form.semester,
+          Year: form.year
+        })
+
+        const response = await axios.get(`/api/Reports/Department?${params.toString()}`)
 
         series.value = [{
           name: 'Avg. Performance',
           data: response.data.results
         }]
+
+        showingChart.value = true
       } catch (error) {
         Notify.create({
           type: 'negative',
           message: 'An error occured while fetching the report data',
           caption: error.message
         })
+
+        showingChart.value = false
       } finally {
         loading.value = false
       }
-    })
+    }
+
+    const semesters = ['Fall', 'Spring', 'Summer']
 
     return {
       loading,
 
       series,
-      options
+      options,
+      showingChart,
+
+      form,
+      semesters,
+      onGenerate
     }
   }
 })
