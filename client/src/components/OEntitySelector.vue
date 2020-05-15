fb694f6bf48f61401a0191aac59ef29011b8d30f
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2020-05-15 09:47:13 +0300

    feat(client): Add CEntitySelector component (#40)

---
+++ client/src/components/OEntitySelector.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <q-select
+      v-model="selectedModel"
+      readonly
+      use-chips
+      multiple
+      hide-dropdown-icon
+      :label="entity.displayName(multiple)"
+    >
+      <template v-slot:append>
+        <q-icon name="mdi-selection-search" class="cursor-pointer" @click="isOpen = true" />
+      </template>
+
+      <template v-slot:selected>
+        <div v-if="selectedModel">
+          <q-chip
+            v-for="model in selectedModel"
+            :key="model[entity.Key]"
+            color="grey-9"
+            text-color="white"
+          >{{ display(model) }}</q-chip>
+        </div>
+      </template>
+    </q-select>
+
+    <q-dialog v-model="isOpen" persistent @hide="isOpen = false">
+      <div style="width: 700px; max-width: 80vw;">
+        <o-crud-table
+          :entity="entity"
+          :data="items"
+          :columns="columns"
+          :pagination="pagination"
+          :actions="actionConfig"
+          :selection="multiple ? 'multiple' : 'single'"
+          :selected.sync="selected"
+        ></o-crud-table>
+
+        <div class="row justify-end q-mt-sm">
+          <q-btn color="primary" label="Save" @click="isOpen = false" />
+        </div>
+      </div>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, reactive, computed, watch } from '@vue/composition-api'
+
+import OCrudTable from '../components/OCrudTable'
+
+export default defineComponent({
+  name: 'OEntitySelector',
+  components: {
+    OCrudTable
+  },
+  props: {
+    value: [Object, Array],
+    columns: {
+      required: true,
+      type: Array
+    },
+    entity: {
+      required: true,
+      type: Object
+    },
+    multiple: {
+      type: Boolean,
+      default: false
+    },
+    display: {
+      type: Function,
+      default (model) {
+        return model[this.entity.key]
+      }
+    },
+    sortBy: {
+      type: Object,
+      default () {
+        return {
+          // Find first sortable column
+          field: this.columns.find(column => column.sortable).name,
+          descending: false
+        }
+      }
+    }
+  },
+  setup (props, context) {
+    const isOpen = ref(false)
+
+    const items = ref([])
+    const pagination = reactive({
+      page: 1,
+      sortBy: props.sortBy.field,
+      descending: props.sortBy.descending,
+      rowsPerPage: context.root.$q.screen.xs ? 12 : 24,
+      rowsNumber: 0
+    })
+
+    const actionConfig = {
+      create: {
+        enabled: false
+      },
+      quickEdit: {
+        enabled: false
+      },
+      edit: {
+        enabled: false
+      },
+      delete: {
+        enabled: false
+      }
+    }
+
+    const selected = ref([])
+    const selectedModel = computed(() => {
+      return selected.value
+    })
+
+    watch(selectedModel, value => {
+      context.emit('input', props.multiple ? value : value[0])
+    })
+
+    return {
+      isOpen,
+
+      items,
+      pagination,
+      actionConfig,
+
+      selected,
+      selectedModel
+    }
+  }
+})
+</script>
--- client/src/pages/Reports.vue
+++ client/src/pages/Reports.vue
@@ -8,13 +8,13 @@
           </q-card-section>
 
           <q-card-section>
-            <q-input
-              v-model.number="form.departmentId"
-              label="Deparment Id"
-              type="number"
-              min="1"
-              :loading="loading"
-            ></q-input>
+            <o-entity-selector
+              v-model="form.department"
+              :columns="department.columns"
+              :entity="department.entity"
+              :display="model => model.Name"
+            />
+
             <q-select v-model="form.semester" :options="semesters" label="Semester" />
             <q-input v-model.number="form.year" label="Year" type="number"></q-input>
           </q-card-section>
@@ -42,8 +42,14 @@ import { defineComponent, ref, reactive } from '@vue/composition-api'
 import axios from 'axios'
 import { Notify } from 'quasar'
 
+import OEntitySelector from '../components/OEntitySelector'
+import { ODataApiService } from '../services/ApiService'
+
 export default defineComponent({
   name: 'ReportPage',
+  components: {
+    OEntitySelector
+  },
   setup (props, context) {
     const loading = ref(false)
 
@@ -102,7 +108,7 @@ export default defineComponent({
     }
 
     const form = reactive({
-      departmentId: null,
+      department: null,
       semester: null,
       year: null
     })
@@ -112,7 +118,7 @@ export default defineComponent({
 
       try {
         const params = new URLSearchParams({
-          DepartmentId: form.departmentId,
+          DepartmentId: form.department.Id,
           Semester: form.semester,
           Year: form.year
         })
@@ -140,6 +146,8 @@ export default defineComponent({
 
     const semesters = ['Fall', 'Spring', 'Summer']
 
+    const department = useDepartmentSelector()
+
     return {
       loading,
 
@@ -149,8 +157,43 @@ export default defineComponent({
 
       form,
       semesters,
-      onGenerate
+      onGenerate,
+
+      department: ref(department)
     }
   }
 })
+
+function useDepartmentSelector () {
+  const columns = ref([
+    {
+      name: 'name',
+      label: 'Name',
+      field: 'Name',
+      sortable: true,
+      searchable: true
+    }
+  ])
+
+  const departmentService = new ODataApiService('/api/Department')
+
+  const entity = {
+    key: 'Id',
+    name: 'Department',
+    displayName: (plural = false) => `Department${plural ? 's' : ''}`,
+    route: (key = '') => `/departments/${key}`,
+    service: departmentService,
+    defaultValue () {
+      return {
+        Id: 0,
+        Name: ''
+      }
+    }
+  }
+
+  return {
+    columns,
+    entity
+  }
+}
 </script>
